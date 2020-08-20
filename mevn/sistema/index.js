77f52936d6552d2e2db211fe1ccbc746f37b6132
conejofoz <conejofoz@gmail.com>
2020-08-20 01:12:02 -0300

    alterado de ecma5 para ecma6 parou de funcionar

--- mevn/sistema/index.js
+++ mevn/sistema/index.js
@@ -1,6 +1,14 @@
+//ECMA5
+/* 
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const cors = require('cors') 
+*/
+//PARA ECMA6
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
 
 const app=express();
 app.use(morgan('dev'))
