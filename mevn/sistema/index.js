b30318e07e9972ed1dbf6140e419d3e9bf8c2e8d
conejofoz <conejofoz@gmail.com>
2020-08-20 15:24:03 -0300

    corrigido cors e até arquivos estáticos e pasta public

--- mevn/sistema/index.js
+++ mevn/sistema/index.js
@@ -8,15 +8,28 @@ const cors = require('cors')
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
 
 
 const app=express();
 app.use(morgan('dev'))
-app.use(cors)
+//app.use(cors)
+/* 
+O cors precisa estar dentro de um middleware, isso o professor não menciona no curso
+aprendi com Cesar da Celke
+*/
+app.use((req,res, next)=>{
+    app.use(cors())
+    next()
+})
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.set('port',process.env.PORT || 3000)
 app.listen(app.get('port'),()=>{
     console.log('Server on port ' + app.get('port'))
+    //descobrir a pasta public
+    //console.log("pasta" + __dirname + '\\public')
+    console.log(path.join(__dirname, 'public'))
 })
